b04a20da379f23ee3c6a5f1ab09c446c4eb6f0ed
Grant Ellis <gellis@vision-call.co.uk>
2013-03-20 15:54:34 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChangesMade::Application.config.secret_token = '5365650fde96527b6fab0c5a0faaae1204131037ad2b1e0b5ee11e77fbc8294807003495b73e14c5b2bd6ffea2f8c9303c6a8d102e2b2652495a40b2f59a970e'
